d05eeae37150694e0973e4fc15e9fe590be41b22
ramasasank <ramasasankgudipati@gmail.com>
2025-12-04 23:38:05 +0530

    created an express origin

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,15 @@ import { dirname, join } from 'path';
 import authRoutes from "./routes/auth.js";
 import profileRoutes from "./routes/profile.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: join(__dirname, '.env') });
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
