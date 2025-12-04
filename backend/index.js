55ba71507e19a05bb994b044ca674fb3e09df359
ramasasank <ramasasankgudipati@gmail.com>
2025-12-04 23:35:36 +0530

    add middleware

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import authRoutes from "./routes/auth.js";
+import profileRoutes from "./routes/profile.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
