660cafa77bee05189e52b7ae04c250d7fd3c68dd
ramasasank <ramasasankgudipati@gmail.com>
2025-12-04 23:33:38 +0530

    impoted url and path

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,5 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
